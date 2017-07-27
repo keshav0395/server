4fb1d045cad2c9e1096aa09a162aeae3fae6e537
Keshav <keshav000395@gmail.com>
2017-07-28 02:00:14 +0530

    basic

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group( [ 'prefix' => 'Slave', 'namespace' => 'Slave' ], function () {
+
+	Route::get( "/start", [ "uses" => "SlaveController@Start" ] );
+	Route::get( "/block", [ "uses" => "SlaveController@block" ] );
+	Route::get( "/unblock", [ "uses" => "SlaveController@unblock" ] );
+	Route::get( "/refresh", [ "uses" => "SlaveController@refresh" ] );
+	Route::get( "/Status", [ "uses" => "SlaveController@Status" ] );
+	Route::get( "/export/{count?}", [ "uses" => "SlaveController@export" ] );
+
+
+	Route::get( "/build", [ "uses" => "UpdateController@build" ] );
+	Route::get( "/restore", [ "uses" => "BackupController@restore" ] );
+
+});
+
+Route::group( [ 'prefix' => 'Master', 'namespace' => 'Master' ], function () {
+
+});
